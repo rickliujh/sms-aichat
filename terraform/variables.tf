d3ace92c2863e3ef3503f9f0cfb34e5aafbccca0
Rick Ryan <rick.ryan2@outlook.com>
2025-01-06 00:06:55 +0000

    fix(teffaform): format

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "image_tag" {
-  type     = string
-  nullable = false
+  type        = string
+  nullable    = false
   description = "The tag of the target image for deploying on lambda"
 }
 
